e5063045dadf5ef5c268d8d26042b4c910d78b16
Mark Alexis Cabrera <solesourc3@gmail.com>
2024-04-10 20:14:11 +0800

    Initial Commit 04-10-2024

---
+++ app/src/main/java/com/example/cabrera_markalexis_bsit22a3_scrollablelist/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.example.cabrera_markalexis_bsit22a3_scrollablelist.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    val stringResourceId: Int,
+    val imageResourceId: Int
+)
